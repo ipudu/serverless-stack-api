d3f328970f7704bac08ec1abe8a24630fcdff001
Pu Du <rocketsboy@gmail.com>
2020-05-12 21:40:55 -0500

    adding error logging

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -1,13 +1,19 @@
+import * as debug from './debug-lib';
+
 export default function handler(lambda) {
   return function (event, context) {
     return (
       Promise.resolve()
+        // Start debugger
+        .then(() => debug.init(event, context))
         // Run the Lambda
         .then(() => lambda(event, context))
         // On success
         .then((responseBody) => [200, responseBody])
         // On failure
         .catch((e) => {
+          // Print debug messages
+          debug.flush(e);
           return [500, { error: e.message }];
         })
         // Return HTTP response
@@ -19,6 +25,8 @@ export default function handler(lambda) {
           },
           body: JSON.stringify(body),
         }))
+        // Cleanup debugger
+        .finally(debug.end)
     );
   };
 }
